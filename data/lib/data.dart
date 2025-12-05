adfb74b8be0b565ab9b1e23c4de6905ce1f797b4
DetectiveArravingOnTheScene <rorosoul@mail.ru>
2025-12-05 12:53:24 +0300

    Fix typo

--- data/lib/data.dart
+++ data/lib/data.dart
@@ -4,4 +4,4 @@ export 'di/data_di.dart';
 export 'repositories/repositories.dart';
 export 'mappers/mappers.dart';
 export 'providers/providers.dart';
-export 'entities/emtities.dart';
+export 'entities/entities.dart';
