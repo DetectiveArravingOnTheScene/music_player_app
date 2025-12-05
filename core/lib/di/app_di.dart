e0570c06f1ada6da288f73c00058e795327cb915
RomanYadevich <141751803+DetectiveArravingOnTheScene@users.noreply.github.com>
2025-12-05 12:29:41 +0300

    Revert "Revert"

--- core/lib/di/app_di.dart
+++ core/lib/di/app_di.dart
@@ -1,7 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:navigation/navigation.dart';
 
-final GetIt serviceLocator = GetIt.instance;
+final serviceLocator = GetIt.instance;
 final CoreDependencyInjection coreDependencyInjection =
     CoreDependencyInjection();
 
