f3978436750eecf9f576a88c0b0f6a1197d1dd27
DetectiveArrivingOnTheScene <yadevichr@gmail.com>
2026-01-19 10:01:06 +0300

    remove useless dependency

--- core/lib/di/app_di.dart
+++ core/lib/di/app_di.dart
@@ -1,20 +1,9 @@
 import 'package:get_it/get_it.dart';
-import 'package:navigation/navigation.dart';
 
 final GetIt serviceLocator = GetIt.instance;
 final CoreDependencyInjection coreDependencyInjection =
     CoreDependencyInjection();
 
 class CoreDependencyInjection {
-  void initialize() {
-    _initAppRouter();
-  }
-
-  void _initAppRouter() {
-    serviceLocator.registerSingletonAsync<AppRouter>(
-      () async {
-        return AppRouter();
-      },
-    );
-  }
+  void initialize() {}
 }
