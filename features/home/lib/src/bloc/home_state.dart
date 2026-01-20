0d7ca765288827e34c58911b79f0a71d7217b917
DetectiveArrivingOnTheScene <yadevichr@gmail.com>
2026-01-20 09:23:45 +0300

    Update Home to use Dart pattern matching

--- features/home/lib/src/bloc/home_state.dart
+++ features/home/lib/src/bloc/home_state.dart
@@ -4,14 +4,14 @@ import 'package:domain/domain.dart';
 part 'home_state.freezed.dart';
 
 @freezed
-abstract class HomeState with _$HomeState {
-  const factory HomeState.loading() = _Loading;
+sealed class HomeState with _$HomeState {
+  const factory HomeState.loading() = HomeLoading;
 
   const factory HomeState.success({
     required CollectionModel<TrackModel> trandingTracks,
-  }) = _Success;
+  }) = HomeSuccess;
 
   const factory HomeState.failure({
     required String errorMessage,
-  }) = _Failure;
+  }) = HomeFailure;
 }
