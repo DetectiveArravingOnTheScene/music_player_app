b29d1b92ea3b90ede80f36c0925b302b6690d9d3
DetectiveArrivingOnTheScene <yadevichr@gmail.com>
2026-01-26 10:12:47 +0300

    Move MiniPlayerCard to separate file

--- core_ui/lib/widgets/mini_player.dart
+++ core_ui/lib/widgets/mini_player.dart
@@ -1,8 +1,7 @@
 import 'package:domain/domain.dart';
 import 'package:flutter/material.dart';
 
-import '../core_ui.dart';
-import '../extensions/network_image_or_default.dart';
+import 'mini_player_card.dart';
 
 class SwipeableMiniPlayer extends StatefulWidget {
   final Stream<Duration> positionStream;
@@ -147,7 +146,7 @@ class _SwipeableMiniPlayerState extends State<SwipeableMiniPlayer>
                       if (widget.prevTrack != null)
                         Transform.translate(
                           offset: Offset(_dragOffset - totalShift, 0),
-                          child: _MiniPlayerCard(
+                          child: MiniPlayerCard(
                             onLikeToggle: widget.onLikeToggle,
                             onPauseToggle: widget.onPauseToggle,
                             track: widget.prevTrack!,
@@ -160,7 +159,7 @@ class _SwipeableMiniPlayerState extends State<SwipeableMiniPlayer>
                       if (widget.nextTrack != null)
                         Transform.translate(
                           offset: Offset(_dragOffset + totalShift, 0),
-                          child: _MiniPlayerCard(
+                          child: MiniPlayerCard(
                             onLikeToggle: widget.onLikeToggle,
                             onPauseToggle: widget.onPauseToggle,
                             track: widget.nextTrack!,
@@ -172,7 +171,7 @@ class _SwipeableMiniPlayerState extends State<SwipeableMiniPlayer>
 
                       Transform.translate(
                         offset: Offset(_dragOffset, 0),
-                        child: _MiniPlayerCard(
+                        child: MiniPlayerCard(
                           onLikeToggle: widget.onLikeToggle,
                           onPauseToggle: widget.onPauseToggle,
                           track: widget.currentTrack,
@@ -189,119 +188,6 @@ class _SwipeableMiniPlayerState extends State<SwipeableMiniPlayer>
           },
         );
       },
-    );
-  }
-}
-
-class _MiniPlayerCard extends StatelessWidget {
-  final TrackModel track;
-  final Duration? position;
-  final VoidCallback onPauseToggle;
-  final VoidCallback onLikeToggle;
-  final bool isLiked;
-  final bool isPaused;
-
-  const _MiniPlayerCard({
-    required this.track,
-    required this.position,
-    required this.onPauseToggle,
-    required this.onLikeToggle,
-    required this.isLiked,
-    required this.isPaused,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      decoration: BoxDecoration(
-        color: context.colorScheme.primaryContainer,
-        borderRadius: BorderRadius.circular(8),
-        border: Border(
-          left: BorderSide(color: context.colorScheme.primary, width: 0.5),
-          right: BorderSide(color: context.colorScheme.primary, width: 0.5),
-          top: BorderSide(color: context.colorScheme.primary, width: 0.5),
-        ),
-        boxShadow: const <BoxShadow>[
-          BoxShadow(
-            color: Colors.black45,
-            blurRadius: 10,
-            offset: Offset(0, 4),
-          ),
-        ],
-      ),
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            child: Row(
-              children: <Widget>[
-                // Cover
-                Container(
-                  width: 64,
-                  color: context.colorScheme.surface,
-                  child: networkImageOrDefault(track.artworkUrl),
-                ),
-                const SizedBox(width: 12),
-                // Info
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        track.title,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14,
-                          color: context.colorScheme.onPrimaryFixedVariant,
-                        ),
-                      ),
-                      const SizedBox(height: 4),
-                      Text(
-                        track.artist.username,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: context.colorScheme.onPrimaryFixedVariant,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(
-                    isLiked ? Icons.favorite : Icons.favorite_border,
-                  ),
-                  onPressed: onLikeToggle,
-                ),
-                IconButton(
-                  icon: Icon(
-                    isPaused ? Icons.play_arrow_rounded : Icons.pause,
-                    color: context.colorScheme.onPrimaryFixedVariant,
-                  ),
-                  onPressed: onPauseToggle,
-                ),
-                const SizedBox(width: 4),
-              ],
-            ),
-          ),
-          // Duration Line
-          SizedBox(
-            height: 2,
-            child: LinearProgressIndicator(
-              color: context.colorScheme.primary,
-              value: position == null
-                  ? 0
-                  : position!.inSeconds / (track.duration / 1000),
-              backgroundColor: Colors.transparent,
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ core_ui/lib/widgets/mini_player_card.dart
@@ -0,0 +1,117 @@
+import 'package:domain/domain.dart';
+import 'package:flutter/material.dart';
+import '../core_ui.dart';
+import '../extensions/network_image_or_default.dart';
+
+class MiniPlayerCard extends StatelessWidget {
+  final TrackModel track;
+  final Duration? position;
+  final VoidCallback onPauseToggle;
+  final VoidCallback onLikeToggle;
+  final bool isLiked;
+  final bool isPaused;
+
+  const MiniPlayerCard({
+    required this.track,
+    required this.position,
+    required this.onPauseToggle,
+    required this.onLikeToggle,
+    required this.isLiked,
+    required this.isPaused,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        color: context.colorScheme.primaryContainer,
+        borderRadius: BorderRadius.circular(8),
+        border: Border(
+          left: BorderSide(color: context.colorScheme.primary, width: 0.5),
+          right: BorderSide(color: context.colorScheme.primary, width: 0.5),
+          top: BorderSide(color: context.colorScheme.primary, width: 0.5),
+        ),
+        boxShadow: const <BoxShadow>[
+          BoxShadow(
+            color: Colors.black45,
+            blurRadius: 10,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                // Cover
+                Container(
+                  width: 64,
+                  color: context.colorScheme.surface,
+                  child: networkImageOrDefault(track.artworkUrl),
+                ),
+                const SizedBox(width: 12),
+                // Info
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        track.title,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                          color: context.colorScheme.onPrimaryFixedVariant,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        track.artist.username,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: context.colorScheme.onPrimaryFixedVariant,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    isLiked ? Icons.favorite : Icons.favorite_border,
+                  ),
+                  onPressed: onLikeToggle,
+                ),
+                IconButton(
+                  icon: Icon(
+                    isPaused ? Icons.play_arrow_rounded : Icons.pause,
+                    color: context.colorScheme.onPrimaryFixedVariant,
+                  ),
+                  onPressed: onPauseToggle,
+                ),
+                const SizedBox(width: 4),
+              ],
+            ),
+          ),
+          // Duration Line
+          SizedBox(
+            height: 2,
+            child: LinearProgressIndicator(
+              color: context.colorScheme.primary,
+              value: position == null
+                  ? 0
+                  : position!.inSeconds / (track.duration / 1000),
+              backgroundColor: Colors.transparent,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
